5677db5ee9c12860b0d879e8c05ccbd3c7beea47
masashimorita <masashi.morita.mm@gmail.com>
2025-03-04 22:05:55 +0900

    :sparklse: Implement to convert page HTML data to vectors using OpenAI embedding

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,5 +1,13 @@
-import puppeteer from 'puppeteer';
+import dotenv from "dotenv";
 import { PuppeteerWebBaseLoader, Browser, Page } from '@langchain/community/document_loaders/web/puppeteer';
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const {
+  OPENAI_API_KEY,
+} = process.env;
 
 const scrapeUrls = [
     "https://ja.wikipedia.org/wiki/Category:2024%E5%B9%B4%E3%81%AE%E3%83%86%E3%83%AC%E3%83%93%E3%82%A2%E3%83%8B%E3%83%A1",
@@ -33,7 +41,36 @@ const scrapePage = async () => {
   return pageData;
 };
 
+
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 512,
+  chunkOverlap: 100,
+});
+
+const convertVector = async (pageData: string[]) => {
+  const vectors = [] as Number[][];
+  const chunks = [] as string[];
+  for (const page of pageData) {
+    const pageChunks = await splitter.splitText(page);
+    for await (const chunk of pageChunks) {
+      const embedding = await openai.embeddings.create({
+        model: 'text-embedding-3-small',
+        input: chunk,
+        encoding_format: 'float',
+      });
+
+      const vector = embedding.data[0].embedding;
+      console.log(vector);
+      vectors.push(vector);
+      chunks.push(chunk);
+    }
+  }
+  return { vectors, chunks };
+};
+
+
 (async () => {
-  const data = await scrapePage();
-  console.log(data);
+  const pageData = await scrapePage();
+  await convertVector(pageData);
 })();
