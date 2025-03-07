ef10bf7c1f79fb6ad346a99e59be99bc61249fa2
masashimorita <masashi.morita.mm@gmail.com>
2025-03-07 20:39:11 +0900

    :sparkles: Implemented a chat API to reply message with ChatGPT using vectored data

--- client/src/app/api/chat/route.ts
+++ client/src/app/api/chat/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import OpenAI from "openai";
 import { streamText } from "ai";
 import { openai } from "@ai-sdk/openai";
@@ -17,13 +18,50 @@ const pinecone = new Pinecone({ apiKey: PINECONE_API_KEY! });
 const pineconeIndex = pinecone.index(PINECONE_INDEX_NAME!, PINECONE_INDEX_HOST);
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
-  const latestMessage = messages[messages.length - 1]?.content;
+  try {
+    const { messages } = await req.json();
+    const latestMessage = messages[messages.length - 1]?.content;
 
-  let docContext = "";
-  const embeddings = await openAIClient.embeddings.create({
-    model: "text-embedding-3-small",
-    input: latestMessage,
-    encoding_format: 'float',
-  });
+    let docContext = "";
+    const embeddings = await openAIClient.embeddings.create({
+      model: "text-embedding-3-small",
+      input: latestMessage,
+      encoding_format: 'float',
+    });
+
+    const query = {
+      vector: embeddings.data[0].embedding,
+      topK: 10,
+      includeMetadata: true,
+    };
+
+    const vectors = await pineconeIndex.query(query);
+    vectors.matches.forEach(match => docContext += match.metadata);
+
+    const template = {
+      role: "system",
+      content: `
+        あなたはアニメについて詳しいです。
+        コンテキストで受け取った情報を元に、アニメについての質問に答えることができます。
+        これらのコンテキストは最近のWikiページから抽出されました。
+        もしない情報がある場合はあなたの情報を使わないでください。
+        レスポンスに画像は含めないでください。
+        ----------------
+        ${docContext}
+        ----------------
+        Questions: ${latestMessage}
+        ----------------
+  
+      `,
+    };
+
+    const result = await streamText({
+      model: openai('gpt-4o-mini'),
+      prompt: template.content,
+    });
+
+    return result.toDataStreamResponse();
+  } catch (err) {
+    return NextResponse.json({ message: 'error occurred...'}, { status: 400 });
+  }
 }
